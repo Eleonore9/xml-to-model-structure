b720a6193958c0df9244946cbb6352f20d24229f
Eleonore Mayola (Eleonore9) <eleonore.mayola@ymail.com>
2017-03-17 16:38:03 +0000

    Create project in right location + add files needed

--- src/parse_xml/core.clj
+++ src/parse_xml/core.clj
@@ -6,6 +6,7 @@
             [clojure.java.shell :refer [sh with-sh-dir]])
   (:gen-class))
 
+;; Process the XML diagram
 (defn clean-text
   "Try and anticipate special characters in the diagram boxes."
   [text]
@@ -39,6 +40,7 @@
                       :type :box
                       :value (clean-text (:value attrs))})))))))
 
+;; Work out the structure of the model
 (defn create-catalog
   "Create a catalog for the model and differentiate between
    input, function and output."
@@ -90,12 +92,28 @@
                                   (and (not-empty f) (empty? t)) (create-catalog box))))
                     boxes)}))
 
+;; Create the model in a new project directory
 (defn create-model-project
   "Create a Leiningen project for the model.
    Takes in a directory where the project will be created,
    and the name for this new project."
   [project-dirpath project-name]
-  (sh "lein" "new" project-name))
+  (let [src-path (io/file project-dirpath project-name "src/"
+                          (s/replace project-name #"-" "_"))
+        test-path (io/file project-dirpath project-name "test/"
+                           (s/replace project-name #"-" "_"))]
+    (info "Creating a new Clojure project" project-name "at" project-dirpath "...")
+    (sh "lein" "new" "app" project-name :dir (io/file project-dirpath))
+    (info "Creating a 'model.clj' namespace to define the model.")
+    (sh "touch" "model.clj" :dir src-path)
+    (info "Creating a 'schemas.clj' namespace to define the data schemas.")
+    (sh "touch" "schemas.clj" :dir src-path)
+    (info "Creating a 'model_test.clj' namespace to validate the model.")
+    (sh "touch" "model_test.clj" :dir test-path)
+    (sh "mkdir" (str test-path "/acceptance/"))
+    (info "Creating a 'workspace_test.clj' namespace to test the model can be run.")
+    (sh "touch" "workspace_test.clj" :dir (io/file test-path "acceptance"))))
+
 
 (defn -main
   [xml-model-diagram]
